3e31f83ac90dedaea0c8aa19cabb5d9768fa1478
Pasan <pasanu@protonmail.com>
2022-03-06 14:39:20 -0500

    Updated absyn files
    Created FunctionDec, updated NameTY to have the constants VOID and INT, cleaned up formatting

--- SampleParser/absyn/NameTy.java
+++ SampleParser/absyn/NameTy.java
@@ -2,6 +2,8 @@ package absyn;
 
 public class NameTy extends Absyn {
 	public int typ;
+	public final static int INT = 0;
+	public final static int VOID = 1;
 
 	public NameTy(int row, int col, int typ) {
 		this.row = row;
