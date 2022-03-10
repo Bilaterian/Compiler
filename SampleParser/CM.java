543328a307c20ffa3088ad0e5aa2f07e4ba8a4c9
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2022-03-09 23:24:41 -0500

    renamed files

--- SampleParser/CM.java
+++ SampleParser/CM.java
@@ -1,20 +1,7 @@
-/*
-  Created by: Fei Song
-  File Name: Main.java
-  To Build: 
-  After the scanner, tiny.flex, and the parser, tiny.cup, have been created.
-    javac Main.java
-  
-  To Run: 
-    java -classpath /usr/share/java/cup.jar:. Main gcd.tiny
-
-  where gcd.tiny is an test input file for the tiny language.
-*/
-
 import java.io.*;
 import absyn.*;
 
-class Main {
+class CM {
   public final static boolean SHOW_TREE = true;
   private static boolean hasA = false;
 
