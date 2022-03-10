ecb5f6833bf106865b1488bf1068798b0d3c46dd
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2022-03-09 20:50:04 -0500

    added -a flag

--- SampleParser/Main.java
+++ SampleParser/Main.java
@@ -16,14 +16,20 @@ import absyn.*;
 
 class Main {
   public final static boolean SHOW_TREE = true;
+  private static boolean hasA = false;
 
   static public void main(String argv[]) {
     /* Start the parser */
+	for(String arg : argv){
+		if(arg.equals("-a")){
+			hasA = true;
+		}
+	}
 	
     try {
       parser p = new parser(new Lexer(new FileReader(argv[0])));
       Absyn result = (Absyn) (p.parse().value);
-      if (SHOW_TREE && result != null) {
+      if (hasA && result != null) {
         System.out.println("The abstract syntax tree is:");
         ShowTreeVisitor visitor = new ShowTreeVisitor();
         result.accept(visitor, 0);
